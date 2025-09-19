957e609302363dac530470e5f3c42b2d27361f49
Suvam-paul145 <suvampersonal555@gmail.com>
2025-09-19 10:43:20 +0530

    home page, API page APi docx page completed and modern animation, transtion, UI has been added

--- ayur-namaste-sync/src/App.tsx
+++ ayur-namaste-sync/src/App.tsx
@@ -11,6 +11,8 @@ import { CuratorDashboard } from "./pages/CuratorDashboard";
 import NotFound from "./pages/NotFound";
 import { User } from "@/lib/mockData";
 import Home from "./pages/Home";
+import DocsPage from "./pages/DocsPage";
+import API from "./pages/API";
 
 const queryClient = new QueryClient();
 
@@ -60,6 +62,9 @@ const App = () => {
               <Route path="/curator" element={getDashboardComponent()} />
               <Route path="*" element={<NotFound />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/docs" element={<DocsPage />} />
+              <Route path="/api" element={<API />} />
+
             </Routes>
           </BrowserRouter>
         </SidebarProvider>
